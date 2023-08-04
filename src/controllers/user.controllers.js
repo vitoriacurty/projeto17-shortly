7e92651cac20a1249ceed3e2c2fd8bbb2330d2ed
vitoriacurty <vitoria.curty@hotmail.com>
2023-08-03 21:30:51 -0300

    implementação do requisito POST /signin

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js"
 import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid"
 
 export async function signUp(req, res) {
     const { name, email, password } = req.body
@@ -12,12 +13,29 @@ export async function signUp(req, res) {
         await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`,
             [name, email, hash]
         )
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
-    res.sendStatus(201)
 }
 
 export async function signIn(req, res) {
+    const { email, password } = req.body
 
+    try {
+        const userExiste = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
+        if (userExiste.rowCount === 0) return res.sendStatus(401)
+
+        const correctPassword = bcrypt.compareSync(password, userExiste.rows[0].password)
+        if (!correctPassword) return res.sendStatus(401)
+
+        const token = uuid()
+        await db.query(`INSERT INTO session ("userId", token) VALUES ($1, $2);`,
+            [userExiste.rows[0].id, token]
+        )
+
+        res.status(200).send({ token })
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
